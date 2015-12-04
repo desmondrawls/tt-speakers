9286d2d1cc070558da5f52bfb53c1bfc65433d51
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2015-12-03 22:01:44 -0500

    Add basic index and show routes.

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 var express = require('express')
 var app = express()
+
 var fs = require('fs')
 var _ = require('lodash')
+var speakers = []
 
-app.get('/', function(req, res){
-    res.send('Speakers are waiting!')
+fs.readFile('speakers.json', {encoding: 'utf8'}, function(err, data){
+    if(err) throw err
+
+    JSON.parse(data).forEach(function (speaker) {
+        speaker.name.full = _.startCase(speaker.name.first + ' ' + speaker.name.last)
+        speakers.push(speaker)
+    })
 })
 
-app.get('/speakers/1', function(req, res){
-    res.send('Speaker: Isaac Asimov')
+app.get('/', function(req, res){
+    var buffer = ''
+
+    speakers.forEach(function(speaker){
+        buffer += '<a href="/speakers/' + speaker.id + '">' + speaker.name.full + '</a><br>'
+    })
+
+    res.send(buffer)
+})
+
+app.get('/speakers/:id', function(req, res){
+    var id = req.params.id
+    var speaker = _.find(speakers, function(speaker){
+        return speaker.id == id
+    })
+    res.send(speaker.name.full)
 })
 
 var server = app.listen(3000, function(){
